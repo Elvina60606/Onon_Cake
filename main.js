424bbac76a76bf21ae8532c615efef49cb8bf33a
Elvina <cat250070@gmail.tw>
2025-10-01 19:42:16 +0800

    保留地址與發票js

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 import "./assets/js/invoiceSelector";
 import "./assets/js/addressSelector";
-import "./assets/js/dateSelector";
 
 console.log('Hello world');
