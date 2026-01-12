238fb1e0256a91a5e89599102c7dc53f35414874
Elvina <cat250070@gmail.tw>
2026-01-12 08:33:42 +0800

    新增Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Home from './pages/Home'
-
+import Header from './component/Header'
 
 export default function App() {
-  return <Home />
-}
+  return (<>
+            <Header />
+            <Home />
+          </>
+  )}
